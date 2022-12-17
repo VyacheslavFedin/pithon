43f75ae7a96410003c1352c3329802a22388b9ee
VyacheslavFedin <111173018+VyacheslavFedin@users.noreply.github.com>
2022-12-17 15:08:37 +1000

    Delete read_data.py

--- read_data.py
+++
@@ -1,9 +0,0 @@
-def read_data_to_text():
-    data = open('data.txt')
-    text_file = data.readlines()
-    #global text
-    text = []
-    for i in text_file:
-        text.append(i) 
-    data.close()
-    return text
