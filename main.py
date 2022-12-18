ef856c4fbcd5a1d8033bd22ce29b7b0332a5629b
VyacheslavFedin <111173018+VyacheslavFedin@users.noreply.github.com>
2022-12-18 15:52:23 +1000

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Updater
-
-
-load_dotenv()
-secret_token = os.getenv('TOKEN')
